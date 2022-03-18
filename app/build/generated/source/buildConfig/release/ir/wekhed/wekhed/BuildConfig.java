9f9501de11512989379eb76d0672c82aa0cd1c8c
bsslinuxinc <getamira@getamiralimanzar.ir>
2022-03-18 12:15:16 +0330

    wekhed apk source code

---
+++ app/build/generated/source/buildConfig/release/ir/wekhed/wekhed/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ir.wekhed.wekhed;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ir.wekhed.wekhed";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 90;
+  public static final String VERSION_NAME = "9.0";
+}
